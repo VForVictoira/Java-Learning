5ed9d4ec1f6368fe2e5f87c3bde88251105af894
vforvictoira <nordesertwolf@outlook.com>
2019-03-23 12:19:59 +0800

    This 关键字的第一次学习

---
+++ Learning/src/ThisTest1/Student.java
@@ -0,0 +1,18 @@
+package ThisTest1;
+
+class Student 
+{
+	    int rollno;
+	    String name;
+	    float fee;
+	    Student(int rollno, String name, float fee) {
+	        this.rollno = rollno;
+	        this.name = name;
+	        this.fee = fee;
+	    }
+	    
+	    void display() {
+	        System.out.println(rollno + " " + name + " " + fee);
+	    	}
+}
+
