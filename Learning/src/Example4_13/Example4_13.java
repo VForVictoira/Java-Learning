071bf54ecf0d1fbb423b6dc3f107329731b39fd3
vforvictoira <nordesertwolf@outlook.com>
2019-03-30 13:42:58 +0800

    方法重载实例

---
+++ Learning/src/Example4_13/Example4_13.java
@@ -0,0 +1,18 @@
+package Example4_13;
+
+public class Example4_13 
+{
+	public static void main(String[] args) 
+	{
+		Circle circle = new Circle();
+		circle.setRadius(196.87);
+		Tixing lader = new Tixing(3,21,9);
+		Student zhang = new Student();
+		System.out.println("zhang计算圆的面积：");
+		double result = zhang.computeArea(circle);
+		System.out.println(result);
+		System.out.println("zhang计算梯形面积：");
+		result = zhang.computeArea(lader);
+		System.out.println(result);
+	}
+}
---
+++ Learning/src/Example4_13/Student.java
@@ -0,0 +1,12 @@
+package Example4_13;
+
+public class Student {
+	double computeArea(Circle c) {
+		double area = c.getArea();
+		return area;
+	}
+	double computeArea(Tixing t) {
+		double area = t.getArea();
+		return area;
+	}
+}
